8618d3ab46977a146914e2a191cce0e2c5590374
Nivetha Kuruparan <nivetha.kuruparan@gmail.com>
2021-12-03 21:50:45 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,104 @@
+import { isValidZip, showAlert } from "./validate";
+
+// require dotenv
+require("dotenv").config();
+
+const petForm = document.querySelector("#pet-form");
+
+petForm.addEventListener("submit", fetchAnimals);
+
+// fetch animals from API
+function fetchAnimals(e) {
+    e.preventDefault();
+
+    // Get user Input
+    const animal = document.querySelector("#animal").value;
+    const zip = document.querySelector("#zip").value;
+
+    // Validate Zip
+    if (!isValidZip(zip)) {
+        showAlert("Please Enter A Valid Zipcode", "danger");
+        return;
+    }
+
+    // fetch Pets
+
+    //let key = process.env.PETFINDER_API_KEY;
+    //let secret = process.env.PETFINDER_API_SECRET_KEY;
+    let token;
+
+    // get authorization token
+    fetch("https://api.petfinder.com/v2/oauth2/token", {
+        method: "POST",
+        body:
+            "grant_type=client_credentials&client_id=mIKxyT19fOouYOOBhjlwxgQOInhmr0YbByiqTT97XvPhR61laY&client_secret=XibAzEcUZV3dD1mEECknjjjTnXqdUNxhToUPTDLW",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            token = data.access_token;
+        })
+        .then(() => {
+            // use token to fetch animals
+            fetch(
+                `https://api.petfinder.com/v2/animals?type=${animal}&location=${zip}`,
+                {
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            )
+                .then((res) => res.json())
+                .then((data) => showAnimals(data.animals));
+        })
+        .catch((err) => console.error(err));
+}
+
+// show listings of pets
+function showAnimals(pets) {
+    const results = document.querySelector("#results");
+
+    // clear results first
+    results.innerHTML = "";
+
+    // loop through pets
+    pets.forEach((pet) => {
+        /* console.log(pet); */
+        // create elements
+        const div = document.createElement("div");
+        div.classList.add("card", "card-body", "mb-3");
+        div.innerHTML = `
+        <div class="row">
+        <div class="col-sm-4">
+        <img class="img-fluid card-img mt-2" src="${pet.photos[0] ? pet.photos[0].medium : "https://i.pinimg.com/564x/af/fb/c9/affbc96be98edecba473e0e630069b3b.jpg"
+            }">
+        </div>
+        <div class="col-sm-8">
+          <h4>${pet.name} (${pet.age})</h4>
+          <p class="text-secondary">${pet.breeds.primary}</p>
+          <p>${pet.contact.address.city}, ${pet.contact.address.state} ${pet.contact.address.postcode
+            }</p>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${pet.contact.phone
+                ? `<li class="list-group-item">Phone: ${pet.contact.phone}</li>`
+                : ``
+            }</li>
+            ${pet.contact.email
+                ? `<li class="list-group-item">Email: ${pet.contact.email}</li>`
+                : ``
+            }
+            <li class="list-group-item">Shelter ID: ${pet.organization_id}</li>
+          </ul>
+        
+        </div>
+        
+      </div>
+    `;
+        results.appendChild(div);
+    });
+}
